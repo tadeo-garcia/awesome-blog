b4375ec37af758be39fb478d70d528f759cdd70c
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2021-01-06 11:45:19 -0600

    blog layout page added to homepage/about page

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return <BlogLayout>This is the About Page</BlogLayout>
+}
+
+export default AboutPage
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,18 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
 
 const HomePage = () => {
-  return (
-    <>
-      <h1>Welcome to the awesome blog.</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </>
-  )
+  return <BlogLayout>This is the Home Page</BlogLayout>
 }
 
 export default HomePage
